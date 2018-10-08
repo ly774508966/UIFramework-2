5102de5372c9a72a42de202ef16b10311b5da198
zhangyx_xyz <zhangyx_xyz@outlook.com>
2018-10-08 20:08:06 +0800

    add custom event system

--- Assets/Scripts/Demo/TestTween.cs
+++ Assets/Scripts/Demo/TestTween.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.EventSystems;
 using Games.UICore;
+using Games.Event;
+using Games.GameDefine;
 
 public class TestTween : MonoBehaviour
 {
@@ -11,6 +13,33 @@ public class TestTween : MonoBehaviour
 
     private void Awake()
     {
+    }
+
+    // Event Test
+    private void OnEnable()
+    {
+        RegisterEvent();
+    }
+
+    private void OnDisable()
+    {
+        UnRegisterEvent();
+    }
+
+    public void RegisterEvent()
+    {
+        EventDispatcher.MainEventManager.AddEventListener<int, int>(GameGlobeVar.EventId.TestEvent, TestMessageBox);
+    }
+
+
+    public void UnRegisterEvent()
+    {
+        EventDispatcher.MainEventManager.RemoveEventListener<int, int>(GameGlobeVar.EventId.TestEvent, TestMessageBox);
+    }
+
+    private void TestMessageBox(int mobile, int unicom)
+    {
+        Debug.Log("[China Mobile phone number:" + mobile + "][China Unicom phone number: " + unicom + "]");
     }
 
     private void Update()
@@ -35,6 +64,10 @@ public class TestTween : MonoBehaviour
         {
             tweener.Flip();
             Debug.Log("Tween Reset!");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EventDispatcher.MainEventManager.TriggerEvent<int, int>(GameGlobeVar.EventId.TestEvent, 10086, 10010);
         }
     }
 
